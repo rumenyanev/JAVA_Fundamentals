227f03c45f60a93c8341eb2d828c9f52d737dea6
rumenyanev <zaispanez@gmail.com>
2019-10-12 22:07:11 +0300

    .

--- src/dataTypesAndVariables_Exercise/PokeMon10Three.java
+++ src/dataTypesAndVariables_Exercise/PokeMon10Three.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class PokeMon10Three {
-    public static void main(String[] args) throws IOException {///80/100 ?
+    public static void main(String[] args) throws IOException {// in judge 80/100 ?!?!
         BufferedReader reader = new BufferedReader
                 (new InputStreamReader(System.in));
 
@@ -31,3 +31,40 @@ public class PokeMon10Three {
         System.out.println(count);
     }
 }
+
+ /*Input
+5
+2
+3
+
+Output
+1
+2
+
+Comments
+N = 5, M = 2, Y = 3.
+We start subtracting M from N.
+N - M = 3. 1 target poked.
+N - M = 1. 2 targets poked.
+N < M.
+We print what has remained of N, which is 1.
+We print the count of targets, which is 2.
+
+Input
+10
+5
+2
+
+Output
+2
+1
+
+Comments
+N = 10, M = 5, Y = 2.
+We start subtracting M from N.
+N - M = 5. (N is still not less than M, they are equal).
+N became EXACTLY 50 % of its original value.
+5 is 50 % from 10. So we divide N by Y.
+N / Y = 5 / 2 = 2. (INTEGER DIVISION).
+
+*/
