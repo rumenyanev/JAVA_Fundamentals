0d4e2a35ea5928642f90d759619d7f299b40afc2
rumenyanev <zaispanez@gmail.com>
2019-11-28 17:28:41 +0200

    .

--- src/objectsAndClassesMoreExercise/carSalesman03/Main.java
+++ src/objectsAndClassesMoreExercise/carSalesman03/Main.java
@@ -1,18 +1,92 @@
 package objectsAndClassesMoreExercise.carSalesman03;
-/*
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader
                 (new InputStreamReader(System.in));
+
+        int numOfDataEngine = Integer.parseInt(reader.readLine());
+        Map<String, Engine> enginelist = new LinkedHashMap<>();
+
+        while (numOfDataEngine-- > 0) {
+            String[] inputDataEngine = reader.readLine().split("\\s+");
+
+            String model = inputDataEngine[0];
+            int power = Integer.parseInt(inputDataEngine[1]);
+
+            Engine engine = new Engine(model, power);
+            if (inputDataEngine.length == 3) {
+                String displacement = inputDataEngine[2];
+                engine.setDisplacement(displacement);
+            } else if (inputDataEngine.length == 4) {
+                String displacement = inputDataEngine[2];
+                engine.setDisplacement(displacement);
+                String efficiency = inputDataEngine[3];
+                engine.setEfficiency(efficiency);
+            }
+            enginelist.putIfAbsent(engine.getModel(), engine);
+        }
+        Map<String, Car> cars = new LinkedHashMap<>();
+        int numOfDataCar = Integer.parseInt(reader.readLine());
+        while (numOfDataCar-- > 0) {
+            String[] inputDataCar = reader.readLine().split("\\s+");
+            String model = inputDataCar[0];
+            String engine = inputDataCar[1];
+            Engine engi = enginelist.get(engine);
+
+            Car car = new Car(model, engi);
+            if (inputDataCar.length == 3) {
+                if(isDigit(inputDataCar[2])) {
+                    int weight = Integer.parseInt(inputDataCar[2]);
+                    car.setWeight(weight);
+                }else {
+                    String color = inputDataCar[2];
+                    car.setColor(color);
+                }
+            } else if (inputDataCar.length == 4) {
+                int weight = Integer.parseInt(inputDataCar[2]);
+                car.setWeight(weight);
+                String color = inputDataCar[3];
+                car.setColor(color);
+            }
+            cars.putIfAbsent(car.getModel(),car);
+
+        }
+        for (Car value : cars.values()) {
+            System.out.println(value);
+        }
+    }
+    private static boolean isDigit(String str){
+        for (int i = 0; i <str.length() ; i++) {
+           char symbol = str.charAt(i);
+        if(Character.isLetter(symbol)){
+            return false;
+        }
+
+        }
+        return true;
     }
 }
 /*Car Salesman
-Define two classes Car and Engine. A Car has a model, engine, weight and color. An Engine has model, power, displacement and efficiency. A Car’s weight and color and its Engine’s displacements and efficiency are optional.
-On the first line, you will read a number N which will specify how many lines of engines you will receive, on each of the next N lines you will receive information about an Engine in the following format “<Model> <Power> <Displacement> <Efficiency>”. After the lines with engines, on the next line you will receive a number M – specifying the number of Cars that will follow, on each of the next M lines information about a Car will follow in the following format “<Model> <Engine> <Weight> <Color>”, where the engine in the format will be the model of an existing Engine. When creating the object for a Car, you should keep a reference to the real engine in it, instead of just the engine’s model, note that the optional properties might be missing from the formats.
-Your task is to print each car (in the order you received them) and its information in the format defined bellow, if any of the optional fields has not been given print "n/a" in its place instead:
+Define two classes Car and Engine.
+A Car has a model, engine, weight and color. An Engine has model, power, displacement and efficiency.
+A Car’s weight and color and its Engine’s displacements and efficiency are optional.
+On the first line, you will read a number N which will specify how many lines of engines you will receive,
+on each of the next N lines you will receive information about an Engine in the following format “<Model> <Power> <Displacement> <Efficiency>”.
+
+After the lines with engines, on the next line you will receive a number M – specifying the number of Cars
+that will follow, on each of the next M lines information about a Car will follow in the following format “<Model> <Engine> <Weight> <Color>”,
+ where the engine in the format will be the model of an existing Engine.
+ When creating the object for a Car, you should keep a reference to the real engine in it,
+ instead of just the engine’s model, note that the optional properties might be missing from the formats.
+Your task is to print each car (in the order you received them) and its information in the format defined bellow,
+ if any of the optional fields has not been given print "n/a" in its place instead:
 <CarModel>:
 <EngineModel>:
 Power: <EnginePower>
